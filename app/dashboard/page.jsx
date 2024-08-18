323834321e70d7b00154d07820b62bb0f4b02fd5
Yohannes Ketema <yohannesketemazeleke@gmail.com>
2024-08-18 05:03:35 +0300

    dashboard loading indicator

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,6 +5,7 @@ import { useUser } from "@clerk/nextjs"
 import { collection, doc, getDoc, setDoc } from "@firebase/firestore"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
+import { motion } from "framer-motion"
 
 import {
   Card,
@@ -21,6 +22,7 @@ import { FaArrowRight } from "react-icons/fa6";
 const Dashboard = () => {
   const {isLoaded, isSignedIn, user} = useUser()
   const [flashcards, setflashcards] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   const router = useRouter()
 
   const handleCardClick = (id) => {
@@ -30,6 +32,7 @@ const Dashboard = () => {
   useEffect(() => {
     async function getFlashcards() {
       if(!user) return
+      setIsLoading(true)
       try {
         const docRef = doc(collection(db, 'users'), user.id)
         const docSnap = await getDoc(docRef)
@@ -41,13 +44,20 @@ const Dashboard = () => {
         }
       } catch(error) {
         console.error(`Failed to fetch flashcards: ${error}`)
+      } finally {
+        setIsLoading(false)
       }
     }
     getFlashcards()
   },[user])
 
+  const dotVariants = {
+    start: { y: "0%" },
+    end: { y: "100%" },
+  };
+
   return (
-    <main className="flex-grow mt-32 mx-4 md:mx-12">
+    <main className="flex flex-col flex-grow mt-32 mx-4 md:mx-12">
      <div className="max-w-[90%]">
       <p className="text-3xl md:text-4xl xl:text-5xl font-medium">Welcome back, <span className="text-primary font-semibold">Yohannes</span></p>
       <p className="text-xl my-4">Create flashcards and continue studying</p>
@@ -55,6 +65,25 @@ const Dashboard = () => {
     <div className="flex flex-row justify-end mt-8">
         <Button><IoAdd className="text-xl mr-1 stroke-2 fill-black"/>Create</Button>
       </div>
+      {isLoading && (
+        <div className="flex gap-2 m-auto items-center justify-center">
+          {[...Array(3)].map((_, index) => (
+            <motion.div
+              key={index}
+              className="w-4 h-4 bg-gray-600 rounded-full"
+              variants={dotVariants}
+              initial="start"
+              animate="end"
+              transition={{
+                repeat: Infinity,
+                duration: 0.8,
+                ease: "easeInOut",
+                delay: index * 0.09,
+              }}
+            />
+          ))}
+        </div>
+      )}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 items-center mt-8">
         {flashcards.map((flashcard, index) => 
           <Card key={index} className="flex flex-col">
