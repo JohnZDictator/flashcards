612cc6669db400670a837427a1f7ec812fc89f08
Yohannes Ketema <yohannesketemazeleke@gmail.com>
2024-08-18 12:44:05 +0300

    updating UI

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -90,7 +90,7 @@ const Dashboard = () => {
           ))}
         </div>
       )}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 m-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-8">
         {flashcards.map((flashcard, index) => 
           <Card key={index} className="flex flex-col justify-between">
             <CardHeader>
